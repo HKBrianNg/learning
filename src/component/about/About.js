ec53beaee8044b307ce1537ff71dd75ecc261e17
Brian Ng <ngshunchiang@hotmail.com>
2022-07-21 18:35:43 +0800

    add about page details

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from '../header/Navbar';
+import { docData } from '../../data/docData';
+import { Container, Box, Stack, Paper, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { grey } from '@mui/material/colors';
 
 function About() {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <>
             <Navbar />
-            <div>About</div>
+            <Container maxWidth='xl'>
+                <Stack display='flex' direction='row'>
+                    <Box sx={{ flex: 0.5, padding: 1 }}>
+                        <Paper padding={3}>
+                            <Typography variant="h4" gutterBottom component="div" align='center'>
+                                <i>
+                                    {docData.title}
+                                </i>
+                            </Typography>
+                            <Typography variant="h6" gutterBottom component="div" sx={{ paddingX: 2 }} >
+                                {docData.summary}
+                            </Typography>
+                            <Typography variant='body1' gutterBottom component="div" sx={{ padding: 2, color: grey[600] }}>
+                                <p>{docData.content}</p>
+                            </Typography>
+                        </Paper>
+                    </Box>
+                    <Box sx={{ flex: 0.5, padding: 1 }}>
+                        {docData.items.map((item) => (
+                            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1bh-content"
+                                    id="panel1bh-header"
+                                >
+                                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                                        {item.title}
+                                    </Typography>
+                                    <Typography sx={{ color: 'text.secondary' }}>{item.summary}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        {item.content}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        ))};
+                    </Box>
+                </Stack>
+            </Container>
         </>
     );
 }
---
+++ src/data/docData.js
@@ -0,0 +1,56 @@
+export const docData =
+{
+    "title": "Application Service",
+    "summary": "This e-book outlines eight steps that any organization should consider when looking to adopt a cloud - native approach to applications.",
+    "content": "Applications have become the way a majority of organizations interact with their customers, partners, and employees.This rapid rise of new digitally native capabilities has disrupted traditional business models and required established companies and industry sectors to adapt and modernize their operations. For the majority of organizations, creating innovative digital experiences means pivoting to a culture of organizational agility, where the rapid pace of demand can only be satisfied by faster and more flexible development and delivery models.But most organizations do not have the luxury of completely rebuilding their technology foundation or immediately adopting new practices and mindsets. Instead, they are embracing gradual yet fundamental shifts in culture, processes, and technology to support greater velocity and agility.",
+    "items": [
+        {
+            "id": "1",
+            "title": "Step 1",
+            "summary": "Evolve culture and practices for the cloud",
+            "content": "The path to cloud-native applications requires development, line-of-business, and IT operations teams to evolve in many different ways to build and deploy apps faster and more efficiently. Regardless of industry or size, every business needs to consider the wide range of activities, technologies, teams, and processes that require collaboration and coordination for the successful deployment of cloud applications. While traditional approaches to using public or hybrid cloud resources, let teams make independent decisions and move quickly, those strategies have also created isolated data and environment processes that make it difficult to innovate. In an era of rapid innovation, the complexity of managing multiple distributed environments, highly customized legacy applications, and new application workloads can create challenges for those organizations developing a unifying cloud strategy for their applications. Often, without an enterprisewide cloud strategy, organizations are left with untapped potential in their application portfolio. The adoption of a collaborative cloud culture relies not just on using new tools and technologies, but also on encouraging people’s willingness and trust to embrace a more integrated and collaborative approach to developing and delivering applications. The culture of open source software projects can be a guide to building a cohesive connected cloud strategy for applications.",
+        },
+        {
+            "id": "2",
+            "title": "Step 2",
+            "summary": "Speed existing applications using microservices",
+            "content": "When embarking on a cloud-native application journey, organizations should not only focus on new development.Many legacy applications are critical to business operations and revenue generation and cannot simply be replaced.Rather, they need to be integrated with and work alongside new cloud - native applications.But how do you speed up an existing monolith? The answer is to move your existing monolithic architecture to a more modular, microservices- based architecture and application programming interface(API) - based communication. Before beginning the onerous task of refactoring monolithic applications into microservices, organizations should first create a solid foundation for their microservices architecture.Moving to a microservices approach does not mean rushing to move everything at once. Break down your monolith into smaller components at your own pace, using a phased approach. Using a phased approach ensures that applications are built following solid design principles and properly defined domain boundaries. This approach supports a more gradual and less risky transition to a microservices architecture, if needed, and sets the foundation for a successful microservices architecture. Applications that are highly dependent on legacy platforms can still be updated and deployed faster by moving the existing monolith to a container-based platform. This shift speeds deployment and delivers a higher return on investment (ROI). Subsequent integrations or features for the monolith can be built using cloud-native techniques and approaches.",
+        },
+        {
+            "id": "3",
+            "title": "Step 3",
+            "summary": "Use application services to speed development",
+            "content": "",
+        },
+        {
+            "id": "4",
+            "title": "Step 4",
+            "summary": "Choose the right tool for the right task",
+            "content": "",
+        },
+        {
+            "id": "5",
+            "title": "Step 5",
+            "summary": "Provide self-service, on-demand infrastructure",
+            "content": "",
+        },
+        {
+            "id": "6",
+            "title": "Step 6",
+            "summary": "Automate IT to accelerate application delivery",
+            "content": "",
+        },
+        {
+            "id": "7",
+            "title": "Step 7",
+            "summary": "Implement continuous delivery and advanced deployment techniques",
+            "content": "",
+        },
+        {
+            "id": "8",
+            "title": "Step 8",
+            "summary": "Evolve a more modular architecture",
+            "content": "",
+        },
+    ],
+}
